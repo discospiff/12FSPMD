c1c8f9ffc46a719c20a4bda43388822ae4310414
Brandan Jones <brandan.jones@gmail.com>
2012-09-03 10:54:26 -0400

    Add basic infrastructure to SpecimenActivity

--- FS2012/src/com/plantplaces/ui/SpecimenActivity.java
+++ FS2012/src/com/plantplaces/ui/SpecimenActivity.java
@@ -1,7 +1,17 @@
+/**
+ * Copyright 2012 PlantPlaces.com
+ */
 package com.plantplaces.ui;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class SpecimenActivity extends Activity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
 }
